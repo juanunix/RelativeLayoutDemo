98eb4d3e1818ea9581e16bd29e75a42d81684781
Manish Srivastava <manishsri01@gmail.com>
2014-05-19 22:52:18 +0530

    first commit

---
+++ gen/com/androidhub4you/relativelayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.androidhub4you.relativelayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/androidhub4you/relativelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.androidhub4you.relativelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
